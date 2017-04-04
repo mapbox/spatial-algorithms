5a1f911656cce5103ece69f7e33c40e9d038ae49
artemp <artem@mapnik.org>
2017-04-04 12:50:48 +0100

    add predicate_visitor

--- include/mapbox/geometry/algorithms/predicates/intersects_impl.hpp
+++ include/mapbox/geometry/algorithms/predicates/intersects_impl.hpp
@@ -102,6 +102,21 @@ struct intersects
     }
 };
 
+template <typename Geometry1>
+struct predicate_visitor
+{
+    predicate_visitor(Geometry1 const& g1_)
+        : g1(g1_) {}
+
+    template <typename Geometry2>
+    bool operator() (Geometry2 const& g2) const
+    {
+        return impl<is_implemented<Geometry1>::value &&
+                    is_implemented<Geometry2>::value>::apply(g1, g2);
+    }
+    Geometry1 const& g1;
+};
+
 template <typename T1, typename T2>
 struct dispatcher
 {
@@ -123,6 +138,26 @@ struct dispatcher
     static bool apply(G1 const& g1, G2 const& g2)
     {
         return detail::intersects() (g1, g2);
+    }
+
+    template <typename G1 = T1, typename G2 = T2, typename std::enable_if
+              <std::is_same<G1, mapbox::geometry::geometry<typename G1::coordinate_type>>::value
+               &&
+               !std::is_same<G2, mapbox::geometry::geometry<typename G2::coordinate_type>>::value
+               ,int>::type = 0>
+    static bool apply(G1 const& g1, G2 const& g2)
+    {
+        return mapbox::util::apply_visitor(detail::predicate_visitor<G2>(g2), g1);
+    }
+
+    template <typename G1 = T1, typename G2 = T2, typename std::enable_if
+              <!std::is_same<G1, mapbox::geometry::geometry<typename G1::coordinate_type>>::value
+               &&
+               std::is_same<G2, mapbox::geometry::geometry<typename G2::coordinate_type>>::value
+               ,int>::type = 0>
+    static bool apply(G1 const& g1, G2 const& g2)
+    {
+        return mapbox::util::apply_visitor(detail::predicate_visitor<G1>(g1), g2);
     }
 };
 
