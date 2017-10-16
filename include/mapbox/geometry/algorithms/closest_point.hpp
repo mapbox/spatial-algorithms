0b13ab69af2772c1f62372bed12279d4713a693b
Artem Pavlenko <artem@mapnik.org>
2017-10-16 12:24:46 +0200

    remove coordinate_type from closest_point_info as we'd expect closest_point to be represented using FP even when input geometries are defined using integer coordinate_type (e.g std::int64_t)

--- include/mapbox/geometry/algorithms/closest_point.hpp
+++ include/mapbox/geometry/algorithms/closest_point.hpp
@@ -2,22 +2,19 @@
 
 namespace mapbox { namespace geometry { namespace algorithms {
 
-template <typename T>
 struct closest_point_info
 {
-    using coordinate_type = T;
     closest_point_info()
-        : x(),y(),distance(-1) {}
-    closest_point_info(coordinate_type x_, coordinate_type y_, double distance_)
+        : x(), y(), distance(-1) {}
+    closest_point_info(double x_, double y_, double distance_)
         : x(x_),
           y(y_),
           distance(distance_) {}
-    coordinate_type x;
-    coordinate_type y;
+    double x;
+    double y;
     double distance;
 };
 
 template <typename T1, typename T2>
-auto closest_point(T1 const& geom, mapbox::geometry::point<T2> const& pt)->closest_point_info<T2>;
-
+closest_point_info closest_point(T1 const& geom, mapbox::geometry::point<T2> const& pt);
 }}}
