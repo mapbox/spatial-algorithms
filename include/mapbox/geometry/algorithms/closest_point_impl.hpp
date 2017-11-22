37a43c4928bb30d65599977edb0400052e7893e9
Artem Pavlenko <artem@mapnik.org>
2017-11-22 15:04:10 +0100

    use boost::geometry::math::equal for floating point equality test (#19)

--- include/mapbox/geometry/algorithms/closest_point_impl.hpp
+++ include/mapbox/geometry/algorithms/closest_point_impl.hpp
@@ -72,7 +72,10 @@ struct closest_point
             {
                 first = false;
                 result = std::move(operator()(geom));
-                if (result.distance == 0.0) return result;
+                if (boost::geometry::math::equals(result.distance, 0.0))
+                {
+                    return result;
+                }
             }
             else
             {
