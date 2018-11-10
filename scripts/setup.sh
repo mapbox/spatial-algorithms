829b76337797fb3f3aa4fa47eb86b8bda2e9a2b1
Dane Springmeyer <dane@mapbox.com>
2018-11-10 15:14:14 -0800

    upgrade to llvm 7

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-v0.14.1}"
-export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-4.0.1}"
+export MASON_RELEASE="${MASON_RELEASE:-master}"
+export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-7.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
 if [[ ${PLATFORM} == 'darwin' ]]; then
